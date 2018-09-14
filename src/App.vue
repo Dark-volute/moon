94aca2abbd4ede366d9f6aa460ee972ca03f9490
shel <4385223434@qq.com>
2018-09-14 16:15:53 +0800

    添加toast

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,44 @@
 <template>
   <div id="app">
-    <m-button :loading='loading' @click='loading = !loading' >按钮</m-button>
-    <m-button icon='settings'>按钮</m-button>
-    <m-button icon='settings' icon-position="right" :loading='true'>按钮</m-button>
+    <!--<m-button :loading='loading' @click='loading = !loading' >按钮</m-button>-->
+    <!--<m-button icon='settings'>按钮</m-button>-->
+    <!--<m-button icon='settings' icon-position="right" :loading='true'>按钮</m-button>-->
+    <button @click="onClickButton">上方弹出</button>
   </div>
 </template>
 
+
+
 <script>
+    import Vue from 'vue'
 import mButton from './components/Button.vue'
 import './components/svg.js'
+import  plugin from '@/components/toast.js'
+
+
+Vue.use(plugin)
 
 export default {
   name: 'app',
-  data(){
-    return {
-        loading: false
-    }
+    data () {
+        return {
+        }
+    },
+    methods: {
+        onClickButton () {
+            this.$toast('hi？', {
+                position:'middle',
+                closeButton: {
+                    text: '关闭',
+                    callback: () => {
+                        console.log('关闭了')
+                    }
+                }
+            })
+        }
+    },
+  mounted(){
+
   },
   components: {
     mButton,
